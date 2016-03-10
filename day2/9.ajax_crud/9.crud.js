b29d867ddf5b6ec415f0ffd21ec4e714dd7e893f
Crystal2030 <867389458@qq.com>
2016-03-10 21:55:53 +0800

    implement the remove functionality

--- day2/9.ajax_crud/9.crud.js
+++ day2/9.ajax_crud/9.crud.js
@@ -35,7 +35,8 @@ var server = http.createServer(function(request,response){
     }else if(pathname == '/list') {
         fs.readFile('./user.json', function (err, data){
             if(!err){
-                response.end('{"status":"success", "value":' + data.toString() + '}');
+                var result = data.toString() == '' ? '[]' : data.toString();
+                response.end('{"status":"success", "value":' + result + '}');
             }else{
                 response.end('Query failed!')
             }
@@ -44,14 +45,25 @@ var server = http.createServer(function(request,response){
     }else if(pathname == '/add'){
         //每当服务器收到客户端发过来的一段数据的时候就会触发data事件
         var str = '';
+        var obj = {};
         request.on('data', function(data){
             str += data.toString();
+            obj = JSON.parse(str);
         });
 
         request.on('end', function(){
             fs.readFile('./user.json', function(err,data){
-                console.log('111',str.toString());
-                users.push(JSON.parse(str.toString()));
+
+                if (data.toString() != '') {
+                    users = JSON.parse(data);
+                }
+
+                if (users.length == 0) {
+                    obj.id = "1";
+                } else {
+                    obj.id = users[users.length - 1].id + 1;
+                }
+                users.push(obj);
                 response.end('{"status":"success", "value":' + str + '}');
 
                 fs.writeFile('./user.json',JSON.stringify(users),[{encoding:'utf-8'},{flag:'a'}], function(err){
@@ -66,17 +78,38 @@ var server = http.createServer(function(request,response){
     }else if(pathname == '/edit'){
 
     }else if(pathname == '/remove'){
-        var id = '';
+        var idstr = '';
         request.on('data', function(data){
-            id = data.toString();
+            idstr = data.toString();
         });
 
         request.on('end', function(){
             fs.readFile('./user.json', function(err, data){
                 if(!err){
-                    console.log('222',id.toString());
-                    //users.push(JSON.parse(str.toString()));
-                    response.end('{"status":"success", "value":' + str + '}');
+                    fs.readFile('./user.json', function(err,data){
+                        if(!err){
+                            if (data.toString() != '') {
+                                users = JSON.parse(data);
+                            }
+                            console.log(idstr);
+                            for(var i=0; i<users.length; i++){
+                                if(users[i].id == idstr){
+                                    console.log(9999);
+                                    users.splice(i,1);
+                                }
+                            }
+                            response.end('{"status":"success", "value":' + JSON.stringify(users) + '}');
+                        }
+                        fs.writeFile('./user.json',JSON.stringify(users),'utf8', function(err){
+                            if(err){
+                                response.end('{"status":"error"}');
+                            }else{
+                                response.end('{"status":"success"}');
+                            }
+                        })
+
+                    })
+
                 }
             })
         })
