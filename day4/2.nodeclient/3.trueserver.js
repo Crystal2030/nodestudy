6c8ce31462ea6367734db9c0175819de9336cf01
Crystal2030 <867389458@qq.com>
2016-03-16 14:35:31 +0800

    wrap trueserver use bodyparser

--- day4/2.nodeclient/3.trueserver.js
+++ day4/2.nodeclient/3.trueserver.js
@@ -1,15 +1,11 @@
 var http = require('http');
 var fs = require('fs');
+var bodyparser = require('./bodyParser');
 var users = [];
 var server = http.createServer(function (req, res) {
-    var result = '';
-    req.on('data',function(data){
-        result+=data;
-    })
-    req.on('end',function(data){
+    bodyparser(req, function(result){
         users.push(JSON.parse(result));
-        //设置响应头，允许哪个来源来访问我这个服务器
         res.end(JSON.stringify(users));
     })
-
 }).listen(6060);
+
