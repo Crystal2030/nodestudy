c49876ee92d8b26bda19cc9a0d12c627ee7880f7
Crystal2030 <867389458@qq.com>
2016-03-17 19:35:35 +0800

    express login and register functionalities

--- day4/3.express/users/app.js
+++ day4/3.express/users/app.js
@@ -5,14 +5,20 @@
  * 2. 用户登陆 登录页面
  * 3. 登陆成功之后返回欢迎页 欢迎页
  */
-//load express
-var express = require('express');
+
 var path = require('path');
+
+var express = require('express');
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');//The flash is a special area of the session used for storing messages
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 var routes = require('./routes/index');
+var settings = require('./settings');
 
 //get config object
-var app = new express();
+var app = express();
 
 //set template engine
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +28,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:false}));
 //set the directory public as static files directory
 app.use(express.static(path.join(__dirname, 'public')));
+//flash
+app.use(flash());
+app.use(session({
+    secret: settings.cookieSecret,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+    store: new MongoStore({
+        url:'mongodb://localhost:27017/myuser',
+        ttl: 14 * 24 * 60 * 60
+    })
+}));
 
+routes(app);
 
-
-
-app.listen(5000);
+app.listen(3000, function(){
+    console.log('Express server listening on port 3000.');
+});
--- day4/3.express/users/routes/index.js
+++ day4/3.express/users/routes/index.js
@@ -1,12 +1,91 @@
 /**
  * Created by crystal on 3/16/16.
  */
-var express = require('express');
-var router = express.Router();
+var User = require('../models/user.js');
 
-//Get home page
-router.get('/', function(req, res){
-    res.render('index', {title: 'Express'});
-});
+module.exports = function(app) {
+    app.get('/', function (req, res) {
+        res.render('index', {
+            title: 'Home',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
 
-module.exports = router;
+        });
+    });
+
+    app.get('/reg', function(req, res){
+        res.render('reg', {
+            title: 'Register',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
+    app.post('/reg', function(req, res){
+        var username = req.body.username,
+            password = req.body.password,
+            confirm_password = req.body['confirm-password'],
+            email = req.body.email;
+        //check password and confirm_password
+        if(password !== confirm_password){
+            req.flash('error', 'The password and its confirm are not the same!');
+            return res.redirect('/reg');
+        }
+
+        var newUser = new User({
+            name: username,
+            password: password,
+            email: email
+        });
+
+        //check whether the user exists or not
+        User.get(newUser.name, function(err, user){
+            if(err){
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            if(user){
+                req.flash('error', 'User already exists!');
+                return res.redirect('/reg');
+            }
+            newUser.save(function(err, user){
+                if(err){
+                    console.log(err);
+                    return res.redirect('/reg');
+                }
+                req.session.user = newUser;//user info store into session
+                req.flash('success', 'Register successfully!')
+                res.redirect('/');
+            })
+        })
+    });
+
+    app.get('/login', function(req, res){
+        res.render('login', {
+            title: 'Login',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
+    app.post('/login', function(req, res){
+        var username = req.body.username,
+            password = req.body.password;
+        User.get(username, function(err, user){
+            if(!user){
+                req.flash('error', 'User not exists!');
+                return res.redirect('/login');
+            }
+            //check whether the password is consistent
+            if(user.password !== password){
+                req.flash('error','Password is not correctly!');
+                return res.redirect('/login');
+            }
+            req.session.user = user;
+            req.flash('success', 'Login successfully!');
+            res.redirect('/');
+        });
+    });
+
+};
