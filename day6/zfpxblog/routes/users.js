17087c00c9de9d3f058eabe4dc5727d10eac0b6e
Crystal2030 <867389458@qq.com>
2016-03-20 16:10:06 +0800

    router

--- day6/zfpxblog/routes/users.js
+++ day6/zfpxblog/routes/users.js
@@ -4,8 +4,26 @@ var router = express.Router();
 /* GET users listing.
 这里的/当前路由下的根目录，不包含一级目录
 */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//用户注册
+router.get('/reg', function(req, res){
+  res.render('user/reg');
 });
+//提交用户注册表单
+router.post('/reg', function(req, res){
+  res.send('reg');
+});
+
+//用户登录
+router.get('/login', function(req, res){
+  res.render('user/login');
+});
+//提交用户登录表单
+router.post('/login', function(req, res){
+  res.send('login');
+});
+
+router.get('/logout', function(req,res){
+  res.render('user/login');
+})
 
 module.exports = router;
