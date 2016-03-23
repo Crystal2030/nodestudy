6147ee572afdb0ff589cbc383f6ea5b5e9afe196
Crystal2030 <867389458@qq.com>
2016-03-23 19:29:43 +0800

    post article

--- day6/201601blog/routes/index.js
+++ day6/201601blog/routes/index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var articleModel = require('../models/article');
+var userModel = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  articleModel.find({}).exec(function(err, docs){
+  articleModel.find({}).populate("'"+userModel.modelName+"'").exec(function(err, docs){
     res.render('index', {
       title: 'Home',
       articles: docs
     });
   });
+
+
 
 });
 
